e25a0845a32927f209045ebdf466d7e1e1264199
nishanthalladi <nishanthalladi01@gmail.com>
2020-09-30 20:00:36 -0400

    Initialize Google Maps client

--- server/main.py
+++ server/main.py
@@ -1,13 +1,18 @@
 from flask import Flask
 import json
+import googlemaps
+
+# Google Maps API client -- everything goes through here
+# email and password for associated account in Discord
+gmaps = googlemaps.Client(key="***REMOVED***")
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # https://localhost:5000/
 def home():
     return "Hey there!"
 
-@app.route('/json-example')
+@app.route('/json-example') # https://localhost:5000/json-example
 def json_example():
     d = {}
     d['example'] = 'value'
