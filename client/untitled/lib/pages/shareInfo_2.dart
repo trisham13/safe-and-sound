ddc6ae8aed0e0652ce7109d01d86c7329bcc97a9
rachelhuang40 <rachelhuang40@gmail.com>
2020-10-13 20:53:30 -0500

    Added the share info page

---
+++ client/untitled/lib/pages/shareInfo_2.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Report';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: Center(
+          child: Column(
+              children: <Widget>[
+                Text(
+                    'Type of crime:',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        height: 3, fontSize: 20),
+                ),
+                ReportBar(),
+                CrimeInfo()
+              ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class ReportBar extends StatefulWidget {
+  ReportBar({Key key}) : super(key: key);
+
+  @override
+  _ReportBarState createState() => _ReportBarState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _ReportBarState extends State<ReportBar> {
+  String dropdownValue = 'Theft';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      //style: TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.black,
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['Theft', 'Assault', 'Burglary', 'Arrest', 'Other']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+class CrimeInfo extends StatefulWidget {
+  CrimeInfo({Key key}) : super(key: key);
+
+  @override
+  _CrimeInfoState createState() => _CrimeInfoState();
+}
+
+class _CrimeInfoState extends State<CrimeInfo> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Location',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Time',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState.validate()) {
+                  // Process data.
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
