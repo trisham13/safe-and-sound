b9403a4b4809580540edd09b759a09fe09142f1f
jiwool4 <70788049+jiwool4@users.noreply.github.com>
2020-10-21 02:43:38 -0400

    Update main.dart
    Working navigation bar

--- client/untitled/lib/main.dart
+++ client/untitled/lib/main.dart
@@ -1,59 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() => runApp(MyApp());
 
+/// This is the main application widget.
 class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildButtonColumn(color, Icons.explore, 'DIRECTIONS'),
-          //_buildButtonColumn(color, Icons.assistant_navigation, 'DIRECTIONS'),
-          _buildButtonColumn(color, Icons.report, 'REPORT'),
-          _buildButtonColumn(color, Icons.vpn_key, 'MY PLACES'),
-          _buildButtonColumn(color, Icons.update, 'UPDATES'),
-          _buildButtonColumn(color, Icons.settings, 'SETTINGS'),
-        ],
-      ),
-    );
     return MaterialApp(
-      title: 'HomePage',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Safe & Sound'),
-        ),
-        body: Column(
-          children: [
-            buttonSection,
-          ],
-        ),
-      ),
+      title: _title,
+      home: MyStatefulWidget(),
     );
-  } // Widget
+  }
+}
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          alignment: Alignment.bottomCenter,
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(color: Color(0xff74a1c3),fontSize: 20, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      '// Index 0: DIRECTIONS',
+      style: optionStyle,
+    ),
+    Text(
+      '// Index 1: REPORT',
+      style: optionStyle,
+    ),
+    Text(
+      '// Index 2: MY PLACES',
+      style: optionStyle,
+    ),
+    Text(
+      '// Index 3: UPDATES',
+      style: optionStyle,
+    ),
+    Text(
+      '// Index 4: SETTINGS',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('S a f e & S o u n d', textAlign: TextAlign.center,
+          style: GoogleFonts.teko(textStyle: TextStyle(fontSize: 30)),),
+            backgroundColor: Color(0xff74a1c3),
+
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'DIRECTIONS',
           ),
-        ),
-      ],
+          BottomNavigationBarItem(
+            icon: Icon(Icons.report),
+            label: 'REPORT',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.vpn_key),
+            label: 'MY PLACES',
+          ),BottomNavigationBarItem(
+            icon: Icon(Icons.update),
+            label: 'UPDATES',
+          ),BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'SETTINGS',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color(0xff74a1c3),
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
