e2a92d8e1df9cace77431240c59950befefc411d
rachelhuang40 <rachelhuang40@gmail.com>
2020-11-18 17:34:23 -0600

    Fixed import

--- client/untitled/lib/pages/shareInfo_2.dart
+++ client/untitled/lib/pages/shareInfo_2.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 void main() => runApp(MyApp());
 
