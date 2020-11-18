cffc20405df06487eb1c920dd12d4ffebb22d15f
nishanth <nishanthalladi01@gmail.com>
2020-11-18 17:37:08 -0500

    Write direction getter connection method

--- client/untitled/lib/services/connector.dart
+++ client/untitled/lib/services/connector.dart
@@ -1,9 +1,11 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 /**
  * adds a crime to the firebase database
  *
  * takes in a description of the crime, the location, and its category
+ *
  * the location should be a list of length 2, with the first item being
  *  latitude, and the second being longitude
  *
@@ -20,5 +22,12 @@ Future<bool> addToFirebase(String crimeDescription, List location,
   return response.statusCode == 200; // 200 means everything went OK
 }
 
-
-
+Future<dynamic> getBestRoutes(List locationFrom, List locationTo) async {
+  var url = 'http://localhost:5000/get-directions?location_from=' +
+      locationFrom[0] + ',' + locationFrom[1] +
+      '&location_to=' + locationTo[0] + ',' + locationTo[1];
+  var response = await http.get(url);
+  // print('Response status: ${response.statusCode}');
+  var jsonOfDirections = json.decode(response.body);
+  return jsonOfDirections;
+}
