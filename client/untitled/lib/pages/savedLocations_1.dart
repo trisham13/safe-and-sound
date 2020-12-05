5455a11f4016d3886882c47aaba86cab204eca67
trisham13 <44246612+trisham13@users.noreply.github.com>
2020-12-05 13:48:36 -0800

    Added Submit button and Shared Preferences

--- client/untitled/lib/pages/savedLocations_1.dart
+++ client/untitled/lib/pages/savedLocations_1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(SavedLocations());
 
@@ -10,14 +11,13 @@ class SavedLocations extends StatefulWidget {
 
 class _SavedLocationsState extends State<SavedLocations> {
 
-  List<Widget> dynamicList;
-  List<String> litem;
+  static List<Widget> dynamicList;
 
   @override
+  // ignore: override_on_non_overriding_member
   initState() {
     super.initState();
     dynamicList = [new LocationWidget()];
-    litem = ["Test1", "Test2", "Test4", "Test5", "Test6", "Test7", "Test8"];
   }
 
   @override
@@ -63,45 +63,65 @@ class _SavedLocationsState extends State<SavedLocations> {
 }
 
 class LocationWidget extends StatelessWidget {
+  
+  final nameController = TextEditingController();
+  final startController = TextEditingController();
+  final endController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Column (
-            children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Name',
-                  hintText: 'Work',
-                ),
-                style: GoogleFonts.teko(textStyle: TextStyle(fontSize: 20)),
-              ),
-              Row(
-                children: [
-                  Flexible(
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Starting Point',
-                        hintText: '196 Tech Way',
-                        prefixIcon: Icon(Icons.location_pin)
-                      ),
-                      style: GoogleFonts.teko(textStyle: TextStyle(fontSize: 20)),
-                    ),
+      child: Column (
+        children: <Widget>[
+          TextField(
+            controller: nameController,
+            decoration: const InputDecoration(
+              labelText: 'Name',
+              hintText: 'Work',
+            ),
+            style: GoogleFonts.teko(textStyle: TextStyle(fontSize: 20)),
+          ),
+          Row(
+            children: [
+              Flexible(
+                child: TextField(
+                  controller: startController,
+                  decoration: const InputDecoration(
+                    labelText: 'Starting Point',
+                    hintText: '196 Tech Way',
+                    prefixIcon: Icon(Icons.location_pin)
                   ),
-                  Flexible(
-                    child: TextFormField(
-                      decoration: const InputDecoration(
-                        labelText: 'Destination',
-                        hintText: '100 Grade Lane',
-                        prefixIcon: Icon(Icons.location_pin)
-                      ),
-                      style: GoogleFonts.teko(textStyle: TextStyle(fontSize: 20)),
-                    ),
-                  )
-                ],
-              )
-            ]
-        )
+                  style: GoogleFonts.teko(textStyle: TextStyle(fontSize: 20)),
+                ),
+              ),
+              Flexible(
+                child: TextField(
+                  controller: endController,
+                  decoration: const InputDecoration(
+                    labelText: 'Destination',
+                    hintText: '100 Grade Lane',
+                    prefixIcon: Icon(Icons.location_pin)
+                  ),
+                  style: GoogleFonts.teko(textStyle: TextStyle(fontSize: 20)),
+                ),
+              ),
+            ],
+          ),
+          OutlinedButton(
+            child: Text("Submit"),
+            onPressed: () async {
+              saveLocation(nameController.text ?? "", startController.text ?? "", endController.text ?? "");
+              SharedPreferences prefs = await SharedPreferences.getInstance();
+              print(prefs.getStringList(nameController.text) ?? "");
+            }
+          )
+        ]
+      )
     );
+  }
+  saveLocation(String name, String start, String end) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> route = <String>[start, end];
+    prefs.setStringList(name, route);
   }
 }
